5f1dad95e2c8d679862d4e3564df2bec227c290a
Christopher <christopher.loubassou@hotmail.fr>
2019-10-20 14:46:36 +0200

    Improve component and gitignore

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2 className="app-mern__welcome">Welcome to the MERN CRUD, start your navigation</h2>
+            <h2 className="app-mern__welcome">Welcome to the MERN CRUD, start your new creation</h2>
         </div>
     )
 }
