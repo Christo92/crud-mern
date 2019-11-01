c08a15d5a5bbd3dcee5a30d86c393e5dfc7e4890
Christopher <christopher.loubassou@hotmail.fr>
2019-11-01 18:33:18 +0100

    Person component change to hooks

--- client/src/components/Person.js
+++ client/src/components/Person.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 
-class Person extends Component {
+function Person(props) {
 
-    handleDelete = async () => {
+    async function handleDelete() {
         try {
-            console.log(this.props.person);
-            await axios.get(`/api/person/delete/${this.props.person._id}`)  
+            await axios.get(`/api/person/delete/${props.person._id}`)
         } catch (err) {
             console.log(err.message)
         }
 
-        window.location.reload(); 
+        window.location.reload();
     }
 
-    render() {
-        return (
-            <div className="personlist-container__block-person">
-                <p className="personlist-container__block-person-firstname">Firsname: <span>{this.props.person.personName}</span></p>
-                <p className="personlist-container__block-person-nickname">Nickname: <span>{this.props.person.personNickName}</span></p>
-                <p className="personlist-container__block-person-description">Description: <span>{this.props.person.personDescription}</span></p>
-                <div className="personlist-container_buttons">
-                    <Link to={`/edit/${this.props.person._id}`} className="personlist-container__block-person-editbutton">Edit</Link>
-                    <button onClick={this.handleDelete} className="personlist-container__block-person-deletebutton">Delete</button>
-                
-                </div>
+    return (
+        <div className="personlist-container__block-person">
+            <p className="personlist-container__block-person-firstname">Firsname: <span>{props.person.personName}</span></p>
+            <p className="personlist-container__block-person-nickname">Nickname: <span>{props.person.personNickName}</span></p>
+            <p className="personlist-container__block-person-description">Description: <span>{props.person.personDescription}</span></p>
+            <div className="personlist-container_buttons">
+                <Link to={`/edit/${props.person._id}`} className="personlist-container__block-person-editbutton">Edit</Link>
+                <button onClick={handleDelete} className="personlist-container__block-person-deletebutton">Delete</button>
+
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 Person.propTypes = {
