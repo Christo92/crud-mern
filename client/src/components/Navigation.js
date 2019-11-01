3b7aca98f40b6f3ead14b989b843be45089f8f87
Christopher <christopher.loubassou@hotmail.fr>
2019-11-01 18:06:57 +0100

    Navigation change to hooks

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route, Link, NavLink } from 'react-router-dom';
 // Components
 import Home from './Home';
 import Edit from './Edit';
 import PersonList from './PersonList';
 
-class Navigation extends Component {
-    render() {
-        return (
-            <div>
-                <header className="header">
-                    <Link to={'/'} className="header__home">CRUD-MERN</Link>
-                    <div className="header__menu">
-                        <NavLink className="header__menu__link" to={'/create'} activeClassName="active">Create</NavLink>
-                    </div>
-                </header>
-                    <Switch>
-                        <Route exact path="/" component={Home} />   
-                        <Route path="/create" component={PersonList} />
-                        <Route path="/edit/:id" component={Edit} />
-                    </Switch>
-            </div>
-        )
-    }
+function Navigation() {
+    return (
+        <div>
+            <header className="header">
+                <Link to={'/'} className="header__home">CRUD-MERN</Link>
+                <div className="header__menu">
+                    <NavLink className="header__menu__link" to={'/create'} activeClassName="active">Create</NavLink>
+                </div>
+            </header>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/create" component={PersonList} />
+                <Route path="/edit/:id" component={Edit} />
+            </Switch>
+        </div>
+    )
 }
 
 export default Navigation;
