a67ad702627031d060c47bd0d20c1c5530093ec1
Christopher <christopher.loubassou@hotmail.fr>
2019-11-01 18:05:07 +0100

    Edit Component change to hooks

--- client/src/components/Edit.js
+++ client/src/components/Edit.js
@@ -1,114 +1,113 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 
-class Edit extends Component {
+function Edit(props) {
 
-    state = {
-        personName: '',
-        personNickName: '',
-        personDescription: ''
-    };
+    // Set hooks states
+    const [personName, setPersonName] = useState('');
+    const [personNickName, setPersonNickName] = useState('');
+    const [personDescription, setPersonDescription] = useState('');
 
-    async componentDidMount() {
-        try {
-            const editPerson = await axios.get(`/api/person/edit/${this.props.match.params.id}`);
-            let editPersonData = editPerson.data;
+    // Http request to edit the person
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const editPerson = await axios.get(`/api/person/edit/${props.match.params.id}`);
+                let editPersonData = editPerson.data;
 
-            this.setState({
-                personName: editPersonData.personName,
-                personNickName: editPersonData.personNickName,
-                personDescription: editPersonData.personDescription
-            });
+                // Update the data with new input value
+                setPersonName(editPersonData.personName);
+                setPersonNickName(editPersonData.personNickName);
+                setPersonDescription(editPersonData.personDescription);
 
-        } catch (err) {
-            console.error(err.message)
+            } catch (err) {
+                console.error(err.message)
+            }
         }
+        fetchData()
+    }, [props])
+
+    // Get the person name
+    function handlePersonName(e) {
+        setPersonName(e.target.value)
     }
 
-    handlePersonName = (e) => {
-        this.setState({
-            personName: e.target.value
-        })
-    };
+    // Get the person nickname
+    function handlePersonNickname(e) {
+        setPersonNickName(e.target.value)
+    }
 
-    handlePersonNickname = (e) => {
-        this.setState({
-            personNickName: e.target.value
-        })
-    };
+    // Get the person description
+    function handlePersonDescription(e) {
+        setPersonDescription(e.target.value)
+    }
 
-    handlePersonDescription = (e) => {
-        this.setState({
-            personDescription: e.target.value
-        })
-    };
-
-    handleSubmit = async (e) => {
+    async function handleSubmit(e) {
         e.preventDefault();
 
+        // Let's store the inputs value
         const obj = {
-            personName: this.state.personName,
-            personNickName: this.state.personNickName,
-            personDescription: this.state.personDescription
+            personName,
+            personNickName,
+            personDescription
         };
 
+        // Http request post to update value
         try {
-            await axios.post(`/api/person/update/${this.props.match.params.id}`, obj);
+            await axios.post(`/api/person/update/${props.match.params.id}`, obj);
         } catch (err) {
             console.log(err.message)
         }
 
         // After update go back to the list
-        this.props.history.push('/create')
+        props.history.push('/create')
     }
 
-    render() {
-        return (
-            <div className="edit-container">
-                <h3 className="edit-container__title">Update person</h3>
-                <form className="edit-container__form" onSubmit={this.handleSubmit}>
+    return (
+        <div className="edit-container">
+            <h3 className="edit-container__title">Update person</h3>
+            <form className="edit-container__form" onSubmit={handleSubmit}>
 
-                    <div className="edit-container__form-group">
-                        <label className="edit-container__label">Person Name:</label>
-                        <input 
-                            type="text"
-                            className="edit-container__input"
-                            value={this.state.personName}
-                            onChange={this.handlePersonName}
-                        />
-                    </div>
+                <div className="edit-container__form-group">
+                    <label className="edit-container__label">Person Name:</label>
+                    <input
+                        type="text"
+                        className="edit-container__input"
+                        value={personName}
+                        onChange={handlePersonName}
+                    />
+                </div>
 
-                    <div className="edit-container__form-group">
-                        <label className="edit-container__label">Person Nickname:</label>
-                        <input 
-                            type="text"
-                            className="edit-container__input"
-                            value={this.state.personNickName}
-                            onChange={this.handlePersonNickname}
-                        />
-                    </div>
+                <div className="edit-container__form-group">
+                    <label className="edit-container__label">Person Nickname:</label>
+                    <input
+                        type="text"
+                        className="edit-container__input"
+                        value={personNickName}
+                        onChange={handlePersonNickname}
+                    />
+                </div>
 
-                    <div className="edit-container__form-group">
-                        <label className="edit-container__label">Person Description:</label>
-                        <input 
-                            type="text"
-                            className="edit-container__input"
-                            value={this.state.personDescription}
-                            onChange={this.handlePersonDescription}
-                        />
-                    </div>
-                    <div className="edit-container__form-group">
-                        <input 
-                            className="edit-container__submit"
-                            type="submit"
-                            value="Update person"
-                        />
-                    </div>
-                </form>
-            </div>
-        )
-    }
+                <div className="edit-container__form-group">
+                    <label className="edit-container__label">Person Description:</label>
+                    <input
+                        type="text"
+                        className="edit-container__input"
+                        value={personDescription}
+                        onChange={handlePersonDescription}
+                    />
+                </div>
+                <div className="edit-container__form-group">
+                    <input
+                        className="edit-container__submit"
+                        type="submit"
+                        value="Update person"
+                    />
+                </div>
+            </form>
+        </div>
+    )
 }
 
 Edit.propTypes = {
